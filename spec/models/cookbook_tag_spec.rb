bbf025d918b0f011c25d05f9de50d6f2dcebe056
Shane Galvin <Sgalvin36@gmail.com>
2025-01-21 13:08:26 -0700

    Added tag, cookbook_tag, and recipe_tag model and tests

---
+++ spec/models/cookbook_tag_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe CookbookTag, type: :model do
+    describe "associations" do
+        it { should belong_to(:cookbook)}
+        it { should belong_to(:tag)}
+    end
+
+end
