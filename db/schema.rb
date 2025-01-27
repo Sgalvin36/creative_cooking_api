e37f7a9fc9595c2b7b130ede384e46247223b163
Shane Galvin <Sgalvin36@gmail.com>
2025-01-27 16:13:53 -0700

    Add rolify, updated user model tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_21_174615) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_27_220644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -105,6 +105,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_21_174615) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "tag_name"
     t.boolean "cuisine"
@@ -126,10 +136,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_21_174615) do
     t.string "last_name"
     t.string "user_name"
     t.string "password_digest"
-    t.integer "role"
     t.string "key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
   add_foreign_key "cookbook_recipes", "cookbooks"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe User, type: :model do
     describe "associations" do
         it { should have_many(:cookbooks) }
         it { should have_many(:user_recipe_modifications) }
+        it { should have_and_belong_to_many(:roles)}
     end
 
     describe "validations" do
         it { should validate_presence_of(:first_name) }
         it { should validate_presence_of(:last_name) }
         it { should validate_uniqueness_of(:user_name) }
-        it { should validate_presence_of(:role) }
         it { should validate_presence_of(:password) }
         it { should allow_value('ABCabc123321!').for(:password) }
         it { should_not allow_value('ABC').for(:password) }
