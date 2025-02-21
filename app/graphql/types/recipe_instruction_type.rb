f99ca442b74979dfc3b314989bb1117b1e0cdbc4
Shane Galvin <Sgalvin36@gmail.com>
2025-02-21 14:58:45 -0700

    Added recipe instructions to recipe query

--- app/graphql/types/recipe_instruction_type.rb
+++ app/graphql/types/recipe_instruction_type.rb
@@ -2,7 +2,6 @@ module Types
     class RecipeInstructionType < Types::BaseObject
         field :id, ID, null:false
         field :instruction, String, null:false
-        field :instruction_step, Integer, null:false
-        field :recipe, Types::RecipeType, null:true
+        field :instruction_step, Integer, null:false, method: :instruction_step
     end
 end
--- app/graphql/types/recipe_type.rb
+++ app/graphql/types/recipe_type.rb
@@ -3,10 +3,11 @@ module Types
         field :id, ID, null: false
         field :name, String, null:false
         field :image, String, null:false
-        field :serving_size, Integer, null:false
+        field :serving_size, Integer, null:false, method: :serving_size
         field :cookbooks, [Types::CookbookType], null:true
         field :tags, [Types::TagType], null:true
         field :recipe_ingredients, [Types::RecipeIngredientType], null:false
-        field :instructions, [Types::RecipeInstructionType], null:false
+        field :recipe_instructions, [Types::RecipeInstructionType], null:true
     end
+
 end
