f33faa662fdbb4b3efa5b7b27749a4f067a4990d
Shane Galvin <Sgalvin36@gmail.com>
2025-06-06 11:37:22 -0600

    Fixed error when connecting api to front end

--- app/graphql/resolvers/one_recipe_resolver.rb
+++ app/graphql/resolvers/one_recipe_resolver.rb
@@ -2,7 +2,7 @@ module Resolvers
     class OneRecipeResolver < Resolvers::BaseResolver
         type [ Types::RecipeType ], null: false
 
-        argument :id, ID, required: true, description "ID of the recipe"
+        argument :id, ID, required: true, description: "ID of the recipe"
 
         def resolve(id:)
             recipe = Recipes.includes(:recipe_instructions, recipe_ingredients: [:measurement, :ingredient]).find(id)
